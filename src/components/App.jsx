7b1b855e3e9cd45373bfe2fed5009a81e0506d49
andriy <evicandrij@gmail.com>
2023-07-04 13:51:03 +0300

    comit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,11 @@ import friends from './Data/friends.json';
 import data from './Data/data.json';
 import user from './Data/user.json';
 import Transactions from './Data/transactions.json';
-import Profile from './Profile/Profile';
-import Statistics from './Statistics/Statistics';
-import FriendList from './FriendList/FriendList';
-import TransactionHistory from './TransactionHistory/TransactionHistory';
+
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
@@ -24,4 +25,3 @@ export const App = () => {
     </>
   );
 };
-export default App;
